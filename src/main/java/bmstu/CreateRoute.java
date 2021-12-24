2c2dbeb7096aabde913d5380d55c360b4035c313
furstepnik <furstepnik@yandex.ru>
2021-12-24 23:36:23 +0300

    Scripted auto-commit on change (2021-12-24 23:36:23) by gitwatch.sh

--- src/main/java/bmstu/CreateRoute.java
+++ src/main/java/bmstu/CreateRoute.java
@@ -49,8 +49,8 @@ public class CreateRoute {
                         );
                     }
                         }
-                ))
-        ));
+                }
+        )));
     }
 
 }
