a943fd4bd84a0281c840de972f93a4c184970763
furstepnik <furstepnik@yandex.ru>
2021-12-24 22:15:10 +0300

    Scripted auto-commit on change (2021-12-24 22:15:10) by gitwatch.sh

--- src/main/java/bmstu/MessageServer.java
+++ src/main/java/bmstu/MessageServer.java
@@ -1,5 +1,13 @@
 package bmstu;
 
+import java.util.ArrayList;
+
 public class MessageServer {
-    
+    private final ArrayList<String> urls;
+    public MessageServer(ArrayList<String> urls) {
+        this.urls = urls;
+    }
+    public ArrayList<String> gerUrls() {
+        return this.urls;
+    }
 }
