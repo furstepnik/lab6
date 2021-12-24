79b81d4e7c6f440bcb95719790b61102e3f060f0
furstepnik <furstepnik@yandex.ru>
2021-12-24 22:38:07 +0300

    Scripted auto-commit on change (2021-12-24 22:38:07) by gitwatch.sh

--- src/main/java/bmstu/CreateRoute.java
+++ src/main/java/bmstu/CreateRoute.java
@@ -5,5 +5,10 @@ public class CreateRoute {
     private final String COUNT = "count";
     private final int TIMEOUT = 10;
     private final Http http;
+    private final ActorRef confActor;
+
+    public CreateRoute(Http http, ActorRef conf) {
+        
+    }
 
 }
