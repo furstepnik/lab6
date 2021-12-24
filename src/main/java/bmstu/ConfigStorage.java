55ab63deb7f53165e4a86de7dc47a3f76b06a3a4
furstepnik <furstepnik@yandex.ru>
2021-12-24 21:47:54 +0300

    Scripted auto-commit on change (2021-12-24 21:47:54) by gitwatch.sh

--- src/main/java/bmstu/ConfigStorage.java
+++ src/main/java/bmstu/ConfigStorage.java
@@ -1,4 +1,11 @@
 package bmstu;
 
 public class ConfigStorage {
+    private ArrayList<String> storageServers;
+
+    @Override
+    public Receive createReceive() {
+        return Receive =Builder.create()
+                
+    }
 }
