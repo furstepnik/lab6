659004f1cbc1fe20e59471698833863a3f9f2627
furstepnik <furstepnik@yandex.ru>
2021-12-24 22:48:51 +0300

    Scripted auto-commit on change (2021-12-24 22:48:51) by gitwatch.sh

--- src/main/java/bmstu/ZookeeperWatcher.java
+++ src/main/java/bmstu/ZookeeperWatcher.java
@@ -3,5 +3,7 @@ package bmstu;
 public class ZookeeperWatcher {
     private final String CONNECET = "127.0.0.1:2181";
     private final String HOST = "127.0.0.1";
+    private final String PATH = "/servers/s";
+    private final Integer SESSION_TIMEOUT = 5000;
     
 }
