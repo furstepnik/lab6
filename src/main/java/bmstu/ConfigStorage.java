92985eca2137321e9ccd711c6794d052be9cc361
furstepnik <furstepnik@yandex.ru>
2021-12-24 21:50:02 +0300

    Scripted auto-commit on change (2021-12-24 21:50:02) by gitwatch.sh

--- src/main/java/bmstu/ConfigStorage.java
+++ src/main/java/bmstu/ConfigStorage.java
@@ -2,13 +2,15 @@ package bmstu;
 
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class ConfigStorage extends AbstractActor{
     private ArrayList<String> storageServers;
 
     @Override
     public Receive createReceive() {
-        return Receive =Builder.create()
+        return Receive = Builder.create()
         .match(MessageServer.class, msg -> this.storageServers=msg.getUrls())
         .match(EmptyMessage.class, msg -> sender().tell(storageServers.get(new Random().nextInt(storageServers.size())), self()))
         .build();
